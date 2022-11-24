8e642e222e1deb8fd401753ad8baaecb590f7af8
Ivan Nieto <ivannieto59@aragon.unam.mx>
2022-11-23 22:52:10 -0600

    Initial Commit

---
+++ shakeAndBangApp/shakeAndBangAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shakeAndBangAppApp.swift
+//  shakeAndBangApp
+//
+//  Created by Ivan Nieto on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct shakeAndBangAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
